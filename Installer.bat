853c52e9490cafe0986b8bfd4dd50587196a289b
MilchZocker (NIP) <38382296+MilchZocker@users.noreply.github.com>
2021-02-01 18:14:54 +0100

    Add files via upload

--- Installer.bat
+++ Installer.bat
@@ -6,8 +6,8 @@ set config_download_unhollower=true
 )
 CLS
 )
-:: Getting all Files
-
+echo [33m---------------------- Pre-Setup -----------------------[0m
+)
 :: Download 7zip.
 
 echo Downloading 7zip...
@@ -17,6 +17,8 @@ echo:
 echo Downloading Experimental UI Installer...
 powershell -Command "Invoke-WebRequest https://codeload.github.com/MilchZocker/ChillouVR-Dark-UI/zip/Auto-Installers -OutFile Installers.zip"
 echo:
+)
+echo [33m---------------------- Decompressing UI... -----------------------[0m
 )
 :: Decompress .zip.
 
@@ -34,19 +36,62 @@ if %errorlevel% neq 0 (
 )	
 del Installers.zip
 )
+echo [33m---------------------- Select Custome UI Typ -----------------------[0m
+)
+:: Selection of UI's
+
+SET choice=
+SET /p choice=Which Version do you want to Install?. 1: MilchZockers [M] or 2: Slime's [S]: 
+IF NOT '%choice%'=='' SET choice=%choice:~0,1%
+IF '%choice%'=='M' GOTO MilchZockers
+IF '%choice%'=='m' GOTO MilchZockers
+IF '%choice%'=='S' GOTO Slimes
+IF '%choice%'=='s' GOTO Slimes
+IF '%choice%'=='' GOTO no
+)
+echo [33m---------------------- Select UI Version -----------------------[0m
+)
+:MilchZockers
 SET choice=
 SET /p choice=Which Version do you want to Install?. 1: Stable [S] or 2: Experimental [E]: 
 IF NOT '%choice%'=='' SET choice=%choice:~0,1%
-IF '%choice%'=='E' GOTO Experimental
-IF '%choice%'=='e' GOTO Experimental
-IF '%choice%'=='S' GOTO Stable
-IF '%choice%'=='s' GOTO Stable
+IF '%choice%'=='E' GOTO Experimental-MilchZocker
+IF '%choice%'=='e' GOTO Experimental-MilchZocker
+IF '%choice%'=='S' GOTO Stable-MilchZocker
+IF '%choice%'=='s' GOTO Stable-MilchZocker
 IF '%choice%'=='' GOTO no
-
+)
 pause
 exit
-
-:Experimental
+)
+echo [33m---------------------- Select UI Version -----------------------[0m
+)
+:Slimes
+SET choice=
+SET /p choice=Which Version do you want to Install?. 1: Stable [S] or 2: Experimental [E]: 
+IF NOT '%choice%'=='' SET choice=%choice:~0,1%
+IF '%choice%'=='E' GOTO Experimental-Slime
+IF '%choice%'=='e' GOTO Experimental-Slime
+IF '%choice%'=='S' GOTO Stable-Slime
+IF '%choice%'=='s' GOTO Stable-Slime
+IF '%choice%'=='' GOTO no
+)
+pause
+exit
+)
+:Experimental-MilchZocker
 start ChillouVR-Dark-UI-Auto-Installers\UIInstallerExperimental.bat
-
-:Stable
+exit
+)
+:Stable-MilchZocker
+start ChillouVR-Dark-UI-Auto-Installers\UIInstallerStable.bat
+exit
+)
+:Experimental-Slime
+start ChillouVR-Dark-UI-Auto-Installers\UiInstallerExperimentalSlime.bat
+exit
+)
+:Stable-Slime
+start ChillouVR-Dark-UI-Auto-Installers\UiInstallerStableSlime.bat
+exit
+)
